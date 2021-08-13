1c96f0c6548fcc1535cf82563b57b93eb4097d64
Sarthak Agarwal <agarwal.sarthak262012@gmail.com>
2021-08-13 20:01:30 +0530

    Dessert Repository

---
+++ shared/src/commonMain/kotlin/com/example/justdesserts/shared/repository/DessertRepository.kt
@@ -0,0 +1,35 @@
+package com.example.justdesserts.shared.repository
+
+import com.example.com.justdesserts.*
+import com.example.com.justdesserts.type.DessertInput
+import com.example.justdesserts.shared.ApolloProvider
+import com.example.justdesserts.shared.cache.*
+import kotlinx.coroutines.flow.single
+
+class DessertRepository(apolloProvider: ApolloProvider) : BaseRepository(apolloProvider) {
+
+    suspend fun getDesserts(page: Int, size: Int): Desserts? {
+        val response = apolloClient.query(GetDessertsQuery(page, size)).execute().single()
+        return response.data?.desserts?.toDesserts()
+    }
+
+    suspend fun getDessert(dessertId: String): DessertDetail? {
+        val response = apolloClient.query(GetDessertQuery(dessertId)).execute().single()
+        return response.data?.dessert?.toDessertDetail()
+    }
+
+    suspend fun newDessert(dessertInput: DessertInput): Dessert? {
+        val response = apolloClient.mutate(NewDessertMutation(dessertInput)).execute().single()
+        return response.data?.createDessert?.toDessert()
+    }
+
+    suspend fun updateDessert(dessertId: String, dessertInput: DessertInput): Dessert? {
+        val response = apolloClient.mutate(UpdateDessertMutation(dessertId, dessertInput)).execute().single()
+        return response.data?.updateDessert?.toDessert()
+    }
+
+    suspend fun deleteDessert(dessertId: String): Boolean? {
+        val response = apolloClient.mutate(DeleteDessertMutation(dessertId)).execute().single()
+        return response.data?.deleteDessert
+    }
+}
